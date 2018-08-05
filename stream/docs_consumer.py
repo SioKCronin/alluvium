33afae7ba1fabdc2c779240d5e75999f74c3868d
SioKCronin <siobhankcronin@gmail.com>
2018-08-04 17:26:27 -0700

    S3 through Kafka consumer of MVP

---
+++ stream/docs_consumer.py
@@ -0,0 +1,21 @@
+import sys
+import json
+import time
+import os
+from datetime import datetime
+from kafka import KafkaConsumer
+
+conn = 'ec2-52-13-241-228.us-west-2.compute.amazonaws.com:9092'
+term = "tired"
+
+if __name__ == "__main__":
+
+    docs = KafkaConsumer('docs', bootstrap_servers=conn)
+    producer = KafkaConsumer('match', bootstrap_servers=conn)
+
+    for msg in docs:
+        data = json.loads(msg.value.decode("utf-8"))
+        print(data['tokens'])
+        if term in data['tokens']:
+            print("Found it in {} ms".format(int(round(time.time() * 1000)) - msg.timestamp))
+            # This is where you would send to Redis the info)
---
+++ stream/docs_producer.py
@@ -0,0 +1,24 @@
+import sys
+import boto3
+import boto
+import json
+import botocore
+from ast import literal_eval
+from smart_open import smart_open, s3_iter_bucket
+from kafka import KafkaProducer
+import time
+
+conn = 'ec2-52-13-241-228.us-west-2.compute.amazonaws.com:9092'
+
+def main():
+
+    producer = KafkaProducer(bootstrap_servers=conn)
+
+    for line in smart_open('s3://alluvium-data/mvp_docs.json'):
+        print(line)
+        producer.send('docs', line)
+        producer.flush()
+        #time.sleep(1)
+
+if __name__ == '__main__':
+    main()
