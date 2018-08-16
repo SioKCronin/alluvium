b69d0cfa6f6f0b0c4a9d99fc424d20ffc09f1cec
SioKCronin <siobhankcronin@gmail.com>
2018-08-16 13:16:14 -0700

    Add compute  monitoring to UI

--- local_demo/src/bolt/query.py
+++ local_demo/src/bolt/query.py
@@ -1,9 +1,7 @@
-from elasticsearch import Elasticsearch
 import sys
 import json
 import time
 import os
-from datetime import datetime
 from kafka import KafkaConsumer
 from elasticsearch import Elasticsearch
 import rethinkdb as r
@@ -13,24 +11,20 @@ import hashlib
 if __name__ == "__main__":
 
     # db connection
-    # conn = r.connect(host='ec2-54-202-215-9.us-west-2.compute.amazonaws.com', port='28015', db='alluvium')
-    conn = r.connect(host='10.0.0.11', port='28015', db='alluvium')
+    conn = r.connect(host='ec2-54-202-215-9.us-west-2.compute.amazonaws.com', port='28015', db='alluvium')
+    #conn = r.connect(host='10.0.0.11', port='28015', db='alluvium')
 
     # Listen to Kafka docs topic
-    c = '10.0.0.14:9092'
+    #c = '10.0.0.14:9092'
+    c = 'ec2-52-13-241-228.us-west-2.compute.amazonaws.com'
     docs = KafkaConsumer('docs', bootstrap_servers=c)
-
-    # List to Kafka query topic
-    c = '10.0.0.14:9092'
 
     # Create Elasticsearch instance
     es = Elasticsearch()
 
     for msg in docs:
-        #print(msg.value.decode("utf-8"))
-        data = json.loads(msg.value.decode("utf-8"))
-        #data = json.loads(msg.value.strip(',').decode("utf-8"))
-        print(data)
+        in_time = time.time()
+        data = json.loads(msg.value.decode("utf-8").strip())
 
         doc = {
             "query" : {
@@ -45,11 +39,9 @@ if __name__ == "__main__":
 
         res = es.search(index="my-index", doc_type="_doc", body=doc)
 
-        print(res)
-
         # Write matches to RethinkDB
         if res['hits']['total'] > 0:
-            match = json.loads(msg.value.decode('utf-8').strip(','))
+            match = json.loads(msg.value.decode('utf-8').strip())
             #print(match)
             for hit in res['hits']['hits']:
                 print("Found one!")
@@ -57,9 +49,9 @@ if __name__ == "__main__":
                     "query_id": hit['_id'],
                     "article": match['article'],
                     "user_id": match['user_id'],
-                    "time": time.time()
+                    "found_time": int((time.time() - in_time)*1000)
                 }]).run(conn)
-                print(found_match)
+                #print("time since entry:", int(time.time()*1000) - msg.timestamp)
 
         else:
             pass
